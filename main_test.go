1342702a95c001d82a96a32be5c759a0372f5ceb
selfup <boudinot.regis@yahoo.com>
2016-03-06 18:04:48 -0700

    include test written

--- main_test.go
+++ main_test.go
@@ -19,6 +19,20 @@ func TestList_Insert_InsertTwoDataPoints(t *testing.T) {
 	list.insert("data2")
 
 	if list.head.nextNode.data != "data2" {
-		t.Fatalf("unexpected head: %v", list.head)
+		t.Fatalf("unexpected node: %v", list.head.nextNode.data)
+	}
+}
+
+func TestList_Insert_InsertTwoDataPoints(t *testing.T) {
+	list := createList()
+
+	list.insert("data1")
+	list.insert("data2")
+	list.insert("data3")
+
+	search := list.include("data2")
+
+	if search != true {
+		t.Fatalf("unexpected bool: %v", search)
 	}
 }

2a03b1ef211246813dcef9ebab0d0bcdb1b56421
selfup <boudinot.regis@yahoo.com>
2016-03-06 18:00:17 -0700

    append beyond head

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package linkedList
 
+import "fmt"
+
 // List is a thing
 type List struct {
 	head *Node
@@ -19,8 +21,18 @@ func createList() *List {
 	return &List{head: &Node{}}
 }
 
+func (l *List) tail(currentNode *Node) *Node {
+	fmt.Println("The data: ", currentNode)
+	if currentNode.nextNode == nil {
+		return currentNode
+	}
+	return l.tail(currentNode.nextNode)
+}
+
 func (l *List) insert(data string) {
 	if l.head.data == "" {
 		l.head.data = data
+	} else {
+		l.tail(l.head).nextNode = &Node{data: data}
 	}
 }
--- main_test.go
+++ main_test.go
@@ -2,12 +2,23 @@ package linkedList
 
 import "testing"
 
-func TestList_CreateNode_CreatingANodeOnTheList(t *testing.T) {
+func TestList_Insert(t *testing.T) {
 	list := createList()
 
 	list.insert("data1")
 
 	if list.head.data != "data1" {
+		t.Fatalf("unexpected head: %v", list.head)
+	}
+}
+
+func TestList_Insert_InsertTwoDataPoints(t *testing.T) {
+	list := createList()
+
+	list.insert("data1")
+	list.insert("data2")
+
+	if list.head.nextNode.data != "data2" {
 		t.Fatalf("unexpected head: %v", list.head)
 	}
 }
